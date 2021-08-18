6ea919f005c21f414f79dade29c8baaeb080dead
Taylor Radtke <taylorradtke@gmail.com>
2021-08-18 08:55:50 -0500

    step 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  extends: [
-    "airbnb",
-    "plugin:import/errors",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["react-hooks", "prettier"],
-  rules: {},
+  extends: ['airbnb', 'airbnb/hooks'],
 };
